0ae596762f5d7085b668a9a01fc8fff1904a2ea2
evgalp <evgeniyalpayev@gmail.com>
2018-12-01 07:43:21 -0800

    transfer

--- app/main.js
+++ app/main.js
@@ -9,10 +9,10 @@ app.on("ready", () => {
     height: 1024
   });
   mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.webContents.openDevTools();
 
   mainWindow.once("ready-to-show", () => {
     mainWindow.show();
+    mainWindow.webContents.openDevTools();
   });
 
   mainWindow.on("closed", () => {
